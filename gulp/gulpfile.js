cc665ce582dc8a04ad95ee87a584ee5683ec1718
Timothy Long <timothydavidlong@gmail.com>
2016-03-18 16:10:49 -0700

    Initial commit.

---
+++ gulp/gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+
+gulp.task('default', ['autoprefixer']);
+
+gulp.task('autoprefixer', function () {
+    var postcss      = require('gulp-postcss');
+    var sourcemaps   = require('gulp-sourcemaps');
+    var autoprefixer = require('autoprefixer');
+ 
+    return gulp.src('./dev/style.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./prod/style.css'));
+});
