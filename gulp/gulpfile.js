2be92ad1f71bd80d329b42cb87c4f37fa73b8164
Timothy Long <timothydavidlong@gmail.com>
2016-03-18 16:14:22 -0700

    Paths to src and dest

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -7,9 +7,9 @@ gulp.task('autoprefixer', function () {
     var sourcemaps   = require('gulp-sourcemaps');
     var autoprefixer = require('autoprefixer');
  
-    return gulp.src('./dev/style.css')
+    return gulp.src('./src/style.css')
         .pipe(sourcemaps.init())
         .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./prod/style.css'));
+        .pipe(gulp.dest('./public'));
 });
